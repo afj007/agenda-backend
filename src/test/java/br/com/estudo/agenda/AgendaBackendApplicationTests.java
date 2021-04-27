ecca3b6f95f07f02793ec5a801ca6526ed4cc531
alan.junior <afj033.aj@gmail.com>
2021-04-27 11:12:07 -0300

    Primeiro commit

---
+++ src/test/java/br/com/estudo/agenda/AgendaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.estudo.agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
